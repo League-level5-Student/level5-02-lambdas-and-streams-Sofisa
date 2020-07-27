39097fcf052132c446fa68ed5e20adb0578db2c7
Sofisa <30541332+Sofisa@users.noreply.github.com>
2020-07-27 11:23:04 -0700

    Lambda Buttons, Lambda Methods (IP)
    halfway through Lambda Methods

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,15 +1,22 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
 	private JButton tellAJoke = new JButton("TELL A JOKE");
+	int five = 5;
+	int four = 4;
+	Random randy = new Random();
 	
 	public LambdaButtons() {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,17 +24,36 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
+
+		// 1. Call the addActionListener methods for each button. Use lambdas
+		// to define to functionality of the buttons.
+		addNumbers.addActionListener(e -> {
+			int num = five + four;
+			JOptionPane.showMessageDialog(null, "5 + 4 = " + num);
+		});
 		
-		//1. Call the addActionListener methods for each button. Use lambdas
-		//   to define to functionality of the buttons.
+		randNumber.addActionListener(e -> {
+			int randomnum = randy.nextInt();
+			JOptionPane.showMessageDialog(null, "random number is " + randomnum);
+		});
 		
+		tellAJoke.addActionListener(e -> {
+			JOptionPane.showMessageDialog(null, "What do you call a cow with no legs? GROUND BEEF");
+		});
+
 		window.setVisible(true);
 		window.pack();
 	}
-	
+
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
-	
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -13,9 +13,25 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
+		printCustomMessage((s) ->{
+			String reverse = "";
+			for(int i = s. length() - 1; i >= 0; i--) {
+				reverse = reverse + s.charAt(i);
+			}
+			System.out.println(reverse);
+		}, "repeat");
 		
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				if (i /2 == 0) {
+					s.toLowerCase();
+				}else {
+					s.toUpperCase();
+				}
+				System.out.println(s);
+			}
+		}, "repeat");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
 		
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
